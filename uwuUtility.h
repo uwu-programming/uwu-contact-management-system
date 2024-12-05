559f5ee5f5ca8af0e9afcead772835e0136ebb02
Sunshine <yewhang2004@gmail.com>
2024-12-06 04:16:26 +0800

    add uwuUtility.h (currently empty, placeholder)

---
+++ uwuUtility.h
@@ -0,0 +1,6 @@
+#ifndef __UWUUTILITY_H_INCLUDED__ // preprocessor directive check if this header file is include (ifndef stands for if not define)
+#define __UWUUTILITY_H_INCLUDED__ // if current header file hasn't been included, define it (source: https://cplusplus.com/forum/articles/10627/)
+
+
+
+#endif // else, do nothing
