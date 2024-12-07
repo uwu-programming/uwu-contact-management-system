291950d5e4eff499ab3e6ac9f6bc5003fec61da1
Sunshine <yewhang2004@gmail.com>
2024-12-08 07:24:41 +0800

    add utility library uwu

---
+++ uwuMain/uwuUtility.c
@@ -0,0 +1,6 @@
+#include "uwuUtility.h"
+
+// check if char x == char y
+boolean checkCharEqual(char x, char y){
+    return (x == y ? uwuTrue : uwuFalse);
+}
--- uwuMain/uwuUtility.h
+++ uwuMain/uwuUtility.h
@@ -1,6 +1,18 @@
 #ifndef __UWUUTILITY_H_INCLUDED__ // preprocessor directive check if this header file is include (ifndef stands for if not define)
 #define __UWUUTILITY_H_INCLUDED__ // if current header file hasn't been included, define it (source: https://cplusplus.com/forum/articles/10627/)
 
+// custom typedef for easier access and referencing
+typedef char* string;
+typedef int boolean;
+typedef int index;
 
+// enumeration for true and false
+enum UwUBoolean{
+    uwuTrue = 1,
+    uwuFalse = 0
+};
+
+// check if char x == char y
+boolean checkCharEqual(char x, char y);
 
 #endif // else, do nothing
