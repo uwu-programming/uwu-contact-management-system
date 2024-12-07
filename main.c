d176844e20b41da3001dbac6bf19e6c03b7bb20d
Sunshine <yewhang2004@gmail.com>
2024-12-07 12:06:08 +0800

    moved files

--- main.c
+++
@@ -1,9 +0,0 @@
-// include the UI header file
-#include "uwuUI.h"
-
-// main
-int main(int argc, char *argv[]){
-    int status = createGTKAPP(argc, argv);
-
-    return status; // return the status after running createGTKAPP
-}
