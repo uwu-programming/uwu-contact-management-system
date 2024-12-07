dc2f1c6e50899888a0735d82cc75e9445d31b0e6
Sunshine <yewhang2004@gmail.com>
2024-12-08 03:10:13 +0800

    add read username & password from csv file and store them inside linked list

--- uwuMain/uwuUserLogin.c
+++ uwuMain/uwuUserLogin.c
@@ -1,13 +1,19 @@
 #include "uwuUserLogin.h"
 
 // function to create a new node
-struct UwUUserNode* createNewNode(char *username, char *password){
+struct UwUUserNode* createNewNode(string username, string password){
     struct UwUUserNode *uwuNewNode = (struct UwUUserNode*)malloc(sizeof(struct UwUUserNode)); // point to newly created node
     struct UwUUserLoginDetail *uwuNewUser = (struct UwUUserLoginDetail*)malloc(sizeof(struct UwUUserLoginDetail)); // new struct for storing the username and password
     
     // assign the username and password to user login detail struct
-    uwuNewUser -> username = username;
-    uwuNewUser -> password = password;
+    size_t i = 0, j = 0;
+    for (; username[i] != '\0'; i++)
+        uwuNewUser -> username[i] = username[i];
+    uwuNewUser -> username[i] = '\0';
+
+    for (; password[j] != '\0'; j++)
+        uwuNewUser -> password[j] = password[j];
+    uwuNewUser -> password[j] = '\0';
 
     // let the new node's user point to the newly created user struct
     uwuNewNode -> user = uwuNewUser;
@@ -16,19 +22,33 @@ struct UwUUserNode* createNewNode(char *username, char *password){
 }
 
 // function to insert a new node (in front)
-void insertNode(struct UwUUserNode **head, char *username, char *password){
+void addNode(string username, string password){
     struct UwUUserNode *uwuNewNode = createNewNode(username, password); // create a new node
-    uwuNewNode -> next = *head; // makes the new node's next points to address of head
-    *head = uwuNewNode; // makes the new node the head
+    uwuNewNode -> next = uwuUserHeadNode; // makes the new node's next points to address of head
+    uwuUserHeadNode = uwuNewNode; // makes the new node the head
+    UwUUserNodeCount++; // increment the node count by 1
+}
+
+// read and store all users from the file
+void readUsersFromFile(string filePath){
+    char username[MAX_CHAR_LENGTH], password[MAX_CHAR_LENGTH];
+    uwuUserLoginFile = fopen(USERS_CSV, READ); // open the file
+
+    if (uwuUserLoginFile != NULL){
+        while(fscanf(uwuUserLoginFile, "%s %s", username, password) == 2){
+            addNode(username, password);
+        }
+    }
 }
 
 int main(){
-    insertNode(&uwuUserHeadNode, "aaaa", "bbbbb");
-    insertNode(&uwuUserHeadNode, "aaaa", "bbbbb");
-    insertNode(&uwuUserHeadNode, "aaaa", "bbbbb");
+    addNode("aaaadwddwd", "bbbbb");
+    addNode( "aaawdda", "bbbbb");
+    addNode("aadwdwdaa", "bbbbb");
+    readUsersFromFile(USERS_CSV);
     struct UwUUserNode *temp = uwuUserHeadNode;
-    while(temp != NULL){
-        printf("%saaaa\n", temp->user->username);
+    while(temp -> next != NULL){
+        printf("%s %s\n", temp->user->username, temp -> user -> password);
         temp = temp -> next;
     }
     return 0;
--- uwuMain/uwuUserLogin.h
+++ uwuMain/uwuUserLogin.h
@@ -3,39 +3,52 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+/*---------------------------------------------------------------*/
 // define constant macro
-// constant path to users.csv
-#define USERS_CSV "users.csv"
 
 #define READ "r" // macro for reading file
 #define WRITE "w" // macro for writing file
 
+// constant path to users.csv
+#define USERS_CSV "users.csv"
+
+// maximum character store
+#define MAX_CHAR_LENGTH 1024
+
 // global variables
 FILE *uwuUserLoginFile; // FILE pointer to point to users.csv
 
+typedef char* string;
+
 // structure to store username and correspond password
 struct UwUUserLoginDetail{
-    char *username;
-    char *password;
+    char username[1024];
+    char password[1024];
 };
 
+/*---------------------------------------------------------------*/
 // linked list for userLoginDetail
 static int UwUUserNodeCount = 0; // keep track of number of node
 
 // node
 struct UwUUserNode{
-    struct UwUUserLoginDetail *user;
-    struct UwUUserNode *next;
+    struct UwUUserLoginDetail *user; // the structure for user
+    struct UwUUserNode *next; // point to next node
 };
 
 // head
 struct UwUUserNode *uwuUserHeadNode = NULL;
 
 // create a new node
-struct UwUUserNode* createNewNode(char *username, char *password);
+struct UwUUserNode* createNewNode(string username, string password);
 
 // insert node into linked list
-void insertNode(struct UwUUserNode **head, char *username, char *password);
+void addNode(string username, string password);
+
+/*---------------------------------------------------------------*/
+// read all the users from the users.csv and store them into a linked list
+void readUsersFromFile(string filePath);
 
 #endif
